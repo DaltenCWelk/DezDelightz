1943bd382bda4029f8e70f8e0dd860dcf2b0829d
Dalten Welk <dez.cage52@gmail.com>
2026-06-27 15:44:53 -0700

    Initial React Redux Storybook setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Logo } from "../Logo/Logo";
+import { NavBar } from "../NavBar/NavBar";
+
+export const Header = ({ dropDowns }) => {
+  return (
+    <nav className="navbar navbar-expanded-lg navbar-dark bg-light py-0">
+      <div className="container-fluid ps-0">
+        <Link to="/">
+          <Logo />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collpase"
+          data-bs-target="#navbarSupportedConted"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <NavBar dropDowns={[{}, {}, {}]} />
+      </div>
+    </nav>
+  );
+};
